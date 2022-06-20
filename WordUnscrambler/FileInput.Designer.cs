644df278ebd3a016ad81be822d77316ed1235d0b
Elizaveta Vygovskaia <101337015@georgebrown.ca>
2022-06-20 01:28:14 -0400

    lets user to input txt file for unscrambling

--- WordUnscrambler/FileInput.Designer.cs
+++ WordUnscrambler/FileInput.Designer.cs
@@ -28,12 +28,182 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FileInput));
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.openFileTextBox = new System.Windows.Forms.TextBox();
+            this.selectedFileLbl = new System.Windows.Forms.Label();
+            this.errorFileOpenLbl = new System.Windows.Forms.Label();
+            this.btnUnscrambleTxFile = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listAddedWords = new System.Windows.Forms.ListBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("OCR A Extended", 15.75F);
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(63)))), ((int)(((byte)(73)))));
+            this.label2.Location = new System.Drawing.Point(24, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(270, 23);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Please select a file";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(251)))), ((int)(((byte)(255)))));
+            this.btnBrowse.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBrowse.Font = new System.Drawing.Font("OCR A Extended", 13.75F);
+            this.btnBrowse.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(63)))), ((int)(((byte)(73)))));
+            this.btnBrowse.Location = new System.Drawing.Point(544, 84);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(88, 34);
+            this.btnBrowse.TabIndex = 10;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = false;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // openFileTextBox
+            // 
+            this.openFileTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(251)))), ((int)(((byte)(255)))));
+            this.openFileTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.openFileTextBox.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.openFileTextBox.Enabled = false;
+            this.openFileTextBox.Font = new System.Drawing.Font("OCR A Extended", 10F);
+            this.openFileTextBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(146)))), ((int)(((byte)(145)))), ((int)(((byte)(151)))));
+            this.openFileTextBox.Location = new System.Drawing.Point(28, 84);
+            this.openFileTextBox.Multiline = true;
+            this.openFileTextBox.Name = "openFileTextBox";
+            this.openFileTextBox.Size = new System.Drawing.Size(498, 34);
+            this.openFileTextBox.TabIndex = 9;
+            // 
+            // selectedFileLbl
+            // 
+            this.selectedFileLbl.AutoSize = true;
+            this.selectedFileLbl.Font = new System.Drawing.Font("Candara Light", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.selectedFileLbl.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(218)))), ((int)(((byte)(215)))), ((int)(((byte)(205)))));
+            this.selectedFileLbl.Location = new System.Drawing.Point(62, 241);
+            this.selectedFileLbl.Name = "selectedFileLbl";
+            this.selectedFileLbl.Size = new System.Drawing.Size(0, 36);
+            this.selectedFileLbl.TabIndex = 11;
+            this.selectedFileLbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.selectedFileLbl.Click += new System.EventHandler(this.selectedFileLbl_Click);
+            // 
+            // errorFileOpenLbl
+            // 
+            this.errorFileOpenLbl.AutoSize = true;
+            this.errorFileOpenLbl.Font = new System.Drawing.Font("Candara Light", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.errorFileOpenLbl.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(163)))), ((int)(((byte)(177)))), ((int)(((byte)(138)))));
+            this.errorFileOpenLbl.Location = new System.Drawing.Point(75, 178);
+            this.errorFileOpenLbl.Name = "errorFileOpenLbl";
+            this.errorFileOpenLbl.Size = new System.Drawing.Size(0, 19);
+            this.errorFileOpenLbl.TabIndex = 12;
+            this.errorFileOpenLbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.errorFileOpenLbl.Click += new System.EventHandler(this.errorFileOpenLbl_Click);
+            // 
+            // btnUnscrambleTxFile
+            // 
+            this.btnUnscrambleTxFile.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(251)))), ((int)(((byte)(255)))));
+            this.btnUnscrambleTxFile.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnUnscrambleTxFile.Font = new System.Drawing.Font("OCR A Extended", 13.75F);
+            this.btnUnscrambleTxFile.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(63)))), ((int)(((byte)(73)))));
+            this.btnUnscrambleTxFile.Location = new System.Drawing.Point(28, 163);
+            this.btnUnscrambleTxFile.Name = "btnUnscrambleTxFile";
+            this.btnUnscrambleTxFile.Size = new System.Drawing.Size(191, 34);
+            this.btnUnscrambleTxFile.TabIndex = 14;
+            this.btnUnscrambleTxFile.Text = "Unscramble";
+            this.btnUnscrambleTxFile.UseVisualStyleBackColor = false;
+            this.btnUnscrambleTxFile.Click += new System.EventHandler(this.btnUnscrambleTxFile_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(251)))), ((int)(((byte)(255)))));
+            this.btnClear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClear.Font = new System.Drawing.Font("OCR A Extended", 13.75F);
+            this.btnClear.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(251)))), ((int)(((byte)(255)))));
+            this.btnClear.Location = new System.Drawing.Point(28, 476);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(88, 34);
+            this.btnClear.TabIndex = 24;
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("OCR A Extended", 15.75F);
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(63)))), ((int)(((byte)(73)))));
+            this.label1.Location = new System.Drawing.Point(24, 241);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 23);
+            this.label1.TabIndex = 23;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // listAddedWords
+            // 
+            this.listAddedWords.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(251)))), ((int)(((byte)(255)))));
+            this.listAddedWords.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listAddedWords.Font = new System.Drawing.Font("OCR A Extended", 15.75F);
+            this.listAddedWords.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(146)))), ((int)(((byte)(145)))), ((int)(((byte)(151)))));
+            this.listAddedWords.FormattingEnabled = true;
+            this.listAddedWords.ItemHeight = 23;
+            this.listAddedWords.Location = new System.Drawing.Point(28, 280);
+            this.listAddedWords.Name = "listAddedWords";
+            this.listAddedWords.Size = new System.Drawing.Size(230, 207);
+            this.listAddedWords.TabIndex = 22;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(298, 228);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(427, 312);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 25;
+            this.pictureBox1.TabStop = false;
+            // 
+            // FileInput
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form2";
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(251)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(717, 552);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listAddedWords);
+            this.Controls.Add(this.btnUnscrambleTxFile);
+            this.Controls.Add(this.errorFileOpenLbl);
+            this.Controls.Add(this.selectedFileLbl);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.openFileTextBox);
+            this.Controls.Add(this.label2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FileInput";
+            this.Text = "FileInput";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.TextBox openFileTextBox;
+        private System.Windows.Forms.Label selectedFileLbl;
+        private System.Windows.Forms.Label errorFileOpenLbl;
+        private System.Windows.Forms.Button btnUnscrambleTxFile;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listAddedWords;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
--- WordUnscrambler/FileInput.cs
+++ WordUnscrambler/FileInput.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,103 @@ namespace WordUnscrambler
 {
     public partial class FileInput : Form
     {
+        public string filename = "";
         public FileInput()
         {
             InitializeComponent();
+        }
+
+        private void selectedFileLbl_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void errorFileOpenLbl_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            string path = pathToSelectedFile();
+            openFileTextBox.Text = path;
+        }
+
+        private string pathToSelectedFile()
+        {
+            var loadDialog = new OpenFileDialog { Filter = "Text File|*.txt", InitialDirectory = @"C:\" };
+            if (loadDialog.ShowDialog() == DialogResult.OK)
+                filename = loadDialog.FileName;
+            return filename;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            // clear list with words in GUI
+            listAddedWords.Items.Clear();
+            label1.Text = "";
+            btnClear.ForeColor = Color.FromArgb(252, 251, 255);
+            btnClear.Text = "";
+            
+
+        }
+
+        private void btnUnscrambleTxFile_Click(object sender, EventArgs e)
+        {
+            // clear list with words in GUI
+            listAddedWords.Items.Clear();
+            printUnscrambledWords();
+            openFileTextBox.Text = "";
+            label1.Text = "Unscrambled words";
+            btnClear.Text = "Clear";
+            btnClear.ForeColor = Color.FromArgb(65, 63, 73);
+
+        }
+        private List<string> addToTempList()
+        {
+            List<String> temp = new List<String>(); // temporary list
+            foreach (string input in File.ReadAllLines(filename)) // loop with words that user added
+            {
+                foreach (string word in File.ReadAllLines("1000words.txt"))
+                {
+
+                    if (sameChars(input, word)) // if words contain same letters 
+                    {
+                        temp.Add(word); // add words from file to temp list
+                    }
+                }
+            }
+            return temp;
+        }
+        private static bool sameChars(string a, string b)
+        {
+            string str1 = string.Concat(a
+                .OrderBy(c => char.ToLower(c)) // sort ignoring case
+                .ThenBy(c => c));
+            string str2 = string.Concat(b
+                .OrderBy(c => char.ToLower(c)) // sort ignoring case
+                .ThenBy(c => c));
+            // check if str1 and str2 are the same
+            if (str1.ToLower() == str2.ToLower())
+                return true;
+            return false;
+        }
+        private void printUnscrambledWords()
+        {
+            List<string> li = addToTempList();
+            foreach (string word in li)
+            {
+                listAddedWords.Items.Add("\r\n" + word); // display in the list box
+            }
+            if (listAddedWords.Items.Count == 0) // if no words found
+            {
+                listAddedWords.Items.Add("\r\n" + "No words were found"); // display message
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
